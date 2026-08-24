ad834a30a468a3371a120a62500cf18984231f3b
kauandragues <kaua.rodrigues1509@gmail.com>
2026-08-24 17:14:14 -0300

    initial commit

---
+++ Limk.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Limk.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ Limk.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Limk.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
